2f026fb01d9bf25a2b9498f5dcf508c44b0bfe84
levicont <victor84lg@mail.ru>
2015-05-14 07:43:50 +0300

    1. Update splash frame

--- src/main/java/com/lvg/weldercenter/ui/SplashSwingFrame.java
+++ src/main/java/com/lvg/weldercenter/ui/SplashSwingFrame.java
@@ -3,25 +3,24 @@ package com.lvg.weldercenter.ui;
 /**
  * Created by Victor on 07.05.2015.
  */
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Toolkit;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
-import javax.swing.JWindow;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class SplashSwingFrame extends JWindow{
 
+    private final String IMAGE_FILE_NAME = "/img/splash_background.jpg";
     private JWindow mainWindow;
     private JLabel lbCaption;
-    private JPanel pnCenter;
+    private BackgroundPanel backgroundPanel;
     private JPanel pnTop;
     private JPanel pnBottom;
     private JButton btClose;
@@ -42,8 +41,10 @@ public class SplashSwingFrame extends JWindow{
     }
 
     private void init(){
-        lbCaption = new JLabel("Loading...");
-        pnCenter = new JPanel();
+        lbCaption = new JLabel(new ImageIcon(IMAGE_FILE_NAME));
+        backgroundPanel = new BackgroundPanel();
+        backgroundPanel.setImageFile(new File(IMAGE_FILE_NAME));
+        backgroundPanel.setLayout(new BorderLayout());
         pnTop = new JPanel();
         pnBottom = new JPanel();
         btClose = new JButton("close");
@@ -56,23 +57,23 @@ public class SplashSwingFrame extends JWindow{
 
         progressBar = new JProgressBar();
 
-        progressBar.setPreferredSize(new Dimension(this.getSize().width-10,7));
+        progressBar.setPreferredSize(new Dimension(this.getSize().width - 10, 7));
         progressBar.setBorderPainted(false);
         progressBar.setMaximum(100);
         progressBar.setValue(10);
         progressBar.setForeground(Color.BLUE);
 
         pnTop.add(btClose);
-        pnCenter.add(lbCaption);
+        backgroundPanel.add(lbCaption);
         pnBottom.add(progressBar);
 
 
         Container pane = this.getContentPane();
 
         pane.add(pnTop, BorderLayout.NORTH);
-        pane.add(pnCenter, BorderLayout.CENTER);
+        pane.add(backgroundPanel, BorderLayout.CENTER);
         pane.add(pnBottom, BorderLayout.SOUTH);
-
+       // pack();
     }
 
     private void setOnScreenCenter(){
@@ -83,5 +84,64 @@ public class SplashSwingFrame extends JWindow{
 
     public JProgressBar getProgressBar() {
         return progressBar;
+    }
+
+    private class BackgroundPanel extends JPanel{
+
+        private BufferedImage bufferedImage;
+        private Image image;
+
+        public BackgroundPanel(){
+            initComponents();
+        }
+
+        private void initComponents(){
+            this.setLayout(null);
+            this.addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    resizedComponents();
+                }
+            });
+        }
+
+        private void resizedComponents(){
+            int w = this.getWidth();
+            int h = this.getHeight();
+            if ((bufferedImage != null) && (w > 0) && (h > 0)) {
+                image = bufferedImage.getScaledInstance(w, h, Image.SCALE_DEFAULT);
+                this.repaint();
+            }
+        }
+
+        public void paint(Graphics g) {
+            if (image != null) {
+                g.drawImage(image, 0, 0, null);
+            }
+            super.paintChildren(g);
+            super.paintBorder(g);
+        }
+
+        public BufferedImage getImage() {
+            return bufferedImage;
+        }
+
+        public void setImage(BufferedImage image) {
+            this.image = image;
+        }
+        public void setImageFile(File imageFile) {
+            try {
+                if (imageFile == null) {
+                    bufferedImage = null;
+                }
+                BufferedImage bi = ImageIO.read(imageFile);
+                bufferedImage = bi;
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            repaint();
+        }
+
+
     }
 }
